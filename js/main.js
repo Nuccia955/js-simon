d5c19ae4f840e49a52d0ea95725d72712a36a161
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-10 14:51:11 +0100

    add rand number list

--- js/main.js
+++ js/main.js
@@ -2,3 +2,42 @@
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+//refs
+const numbers = document.querySelector('.numbers');
+const numbersList = document.createElement('div');
+numbersList.classList.add('main-font-size', 'd-flex', 'justify-content-center');
+numbersList.append(genNumberList(5))
+numbers.append(numbersList);
+
+//set timer
+
+
+
+
+
+
+
+
+
+/*********
+ FUNCTIONS
+**********/
+
+function genRandNum (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * 
+ * @param {number} numEl 
+ * @returns 
+ */
+function genNumberList (numEl) {
+    let list = '';
+    for(let i = 1; i < numEl; i++ ) {
+        list += ` ${genRandNum(1, 100)} -`;
+    }
+    list += genRandNum(1, 100);
+    return list;
+}
